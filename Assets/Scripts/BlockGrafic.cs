2c52a9bf9d19090b362cd8e938a466f08f106239
koala0520 <46417465+koala0520@users.noreply.github.com>
2019-11-24 12:32:14 +0100

    block

---
+++ Assets/Scripts/BlockGrafic.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockGrafic : MonoBehaviour
+{
+    enum Cubeside { BOTTOM, TOP, LEFT, RIGHT, FRONT, BACK };
+    public enum BlockType
+    {
+        GRASS, DIRT, WATER, STONE, LEAVES, WOOD, WOODBASE, SAND, GOLD, BEDROCK, REDSTONE, DIAMOND, NOCRACK,
+        CRACK1, CRACK2, CRACK3, CRACK4, AIR
+    };
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
