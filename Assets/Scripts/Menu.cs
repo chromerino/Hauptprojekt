7e01092562cf2114f26f8c35fe0f3249b71a77ed
DET Labor obsidian <obsidian@det>
2019-11-26 17:31:06 +0100

    Online
    -Photon Bolt Asset hinzugefügt
    - Erste MP Ansätze

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,39 @@
+﻿using UdpKit;
+using UnityEngine;
+
+public class Menu : Bolt.GlobalEventListener
+{
+    public void StartServer()
+    {
+        BoltLauncher.StartServer();
+    }
+
+    public void StartClient()
+    {
+        BoltLauncher.StartClient();
+    }
+
+    public override void BoltStartDone()
+    {
+        if (BoltNetwork.IsServer)
+        {
+            string matchName = "Test Match";
+
+            BoltNetwork.SetServerInfo(matchName, null);
+            BoltNetwork.LoadScene("Main");
+        }
+    }
+
+    public override void SessionListUpdated(UdpKit.Map<System.Guid, UdpKit.UdpSession> sessionList)
+    {
+        foreach (var session in sessionList)
+        {
+            UdpSession photonSession = session.Value as UdpSession;
+
+            if (photonSession.Source == UdpSessionSource.Photon)
+            {
+                BoltNetwork.Connect(photonSession);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/NetworkCallbacks.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[BoltGlobalBehaviour]
+public class NetworkCallbacks : Bolt.GlobalEventListener
+{
+    public override void SceneLoadLocalDone(string scene)
+    {
+        var spawnPosition = new Vector3(0, 0, 0);
+
+        BoltNetwork.Instantiate(BoltPrefabs.Player, spawnPosition, Quaternion.identity);
+    }
+}
