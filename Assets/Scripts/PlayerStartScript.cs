36df5f5f93e577b2dcde385168a866b9046aba08
Kooper16 <56591765+Kooper16@users.noreply.github.com>
2019-12-07 15:28:50 +0100

    teleport
    Teleportiere Spieler zurück zum Start nach 15 Sekunden

---
+++ Assets/Scripts/PlayerStartScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStartScript : MonoBehaviour
+{
+    private float time;
+    private Vector3 startPosition;
+    [SerializeField] private GameObject character;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = character.transform.position;
+        time = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > time + 15)
+        {
+            character.transform.position = startPosition;
+            Destroy(GetComponent<PlayerStartScript>());
+        }
+    }
+}
