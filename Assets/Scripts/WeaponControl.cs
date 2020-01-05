a38a9f7304f8e821c315c2b5206549586811fa76
Someone4250 <56404209+Someone4250@users.noreply.github.com>
2020-01-05 15:18:27 +0100

    sooooome more changes

---
+++ Assets/Scripts/WeaponControl.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WeaponControl : MonoBehaviour
+{
+    public GameObject[] MainWeapons;
+    public GameObject[] SecondaryWeapons;
+    public GameObject[] MeleeWeapons;
+    private int currentMainWeapon;
+    private int currentSecondaryWeapon;
+    private int currentMeleeWeapon;
+    private GameObject currentlyEquippedWeapon;
+    public Button [] MW;
+    public Button[] SW;
+    public Button[] BF;
+    private int currentBuff;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void equipMainWeapon()
+    {
+        currentlyEquippedWeapon = MainWeapons[currentMainWeapon];
+    }
+    public void equipSecondaryWeapon()
+    {
+        currentlyEquippedWeapon = SecondaryWeapons[currentSecondaryWeapon];
+    }
+    public void equipMeleeWeapon()
+    {
+        currentlyEquippedWeapon = MeleeWeapons[currentMeleeWeapon];
+    }
+    
+
+
+     public void freeze(Button but)
+    {
+        but.interactable = false;
+    }
+    public void unfreeze(Button but)
+    {
+        but.interactable = true;
+    }
+    public void unfreeze(Button[] but)
+    {
+        foreach (Button b in but){
+            unfreeze(b);
+        }
+    }
+    public void mw1()
+    {
+        currentMainWeapon = 0;
+        unfreeze(MW);
+        freeze(MW[0]);
+    }
+    public void mw2()
+    {
+        currentMainWeapon = 1;
+        unfreeze(MW);
+        freeze(MW[1]);
+    }
+    public void mw3()
+    {
+        currentMainWeapon = 2;
+        unfreeze(MW);
+        freeze(MW[2]);
+    }
+    public void sw1()
+    {
+        currentSecondaryWeapon = 0;
+        unfreeze(SW);
+        freeze(SW[0]);
+    }
+    public void sw2()
+    {
+        currentSecondaryWeapon = 1;
+        unfreeze(SW);
+        freeze(SW[1]);
+    }
+    public void sw3()
+    {
+        currentSecondaryWeapon = 2;
+        unfreeze(SW);
+        freeze(SW[2]);
+    }
+
+    public void bf1()
+    {
+        currentBuff = 0;
+        unfreeze(BF);
+        freeze(BF[0]);
+    }
+    public void bf2()
+    {
+        currentBuff = 1;
+        unfreeze(BF);
+        freeze(BF[1]);
+    }
+    public void bf3()
+    {
+        currentBuff = 2;
+        unfreeze(BF);
+        freeze(BF[2]);
+    }
+}
--- Assets/Scripts/WeaponScript.cs
+++ Assets/Scripts/WeaponScript.cs
@@ -4,10 +4,40 @@ using UnityEngine;
 
 public class WeaponScript : MonoBehaviour
 {
-    double Damage;
-    double fireCD; //cooldown between shots/attacks (can also  be a knife or another melee weapon)
+    public double Damage;
+    public double fireCD; //cooldown between shots/attacks (can also  be a knife or another melee weapon)
     enum WeaponType {Primary, Secundary, Melee }
-    int magazinSize; //only important if non-melee weapon
-    int ammoMax; //maximum ammunation possible : only important if non-melee weapon
-    double reloadTime; //only important if non-melee weapon
+    public int magazineSize; //only important if non-melee weapon
+    public int ammoInMagazine;
+    public int ammoMax; //maximum ammunation possible : only important if non-melee weapon
+    public int currentAmmo;
+    public double reloadTime; //only important if non-melee weapon
+    public double timeBorder;
+
+
+
+    public void loadBullets(int numberOfBullets)
+    {
+        if (numberOfBullets > currentAmmo)
+        {
+            numberOfBullets = currentAmmo;
+        }
+        if (numberOfBullets+ ammoInMagazine > magazineSize)
+        {
+            numberOfBullets = magazineSize-ammoInMagazine;
+        }
+        currentAmmo -= numberOfBullets;
+        ammoInMagazine += numberOfBullets;
+
+    }
+    public void ResetAmmo()
+    {
+        currentAmmo = ammoMax;
+        ammoInMagazine = magazineSize;
+    }
+    public void MoreAmmo()
+    {
+        currentAmmo = ammoMax+2*magazineSize;
+        ammoInMagazine = magazineSize;
+    }
 }
