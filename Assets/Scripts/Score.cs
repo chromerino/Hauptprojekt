368cc101b2ccb99a2e7c105d78b74d9374a0eeff
koala0520 <46417465+koala0520@users.noreply.github.com>
2020-01-14 15:17:48 +0100

    Score stellen

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+    static private int scoreInt = 0;
+    static public int ScoreInt
+    {
+        get
+        {
+            return scoreInt;
+        }
+        set 
+        {
+            scoreInt = value;
+        }
+    }
+    
+}
