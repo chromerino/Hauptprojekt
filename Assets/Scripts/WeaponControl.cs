96b0f2c045977b98a8782d8940885e7f2adf0733
Kooper16 <56591765+Kooper16@users.noreply.github.com>
2020-01-20 23:04:37 +0100

    Revert "Revert "Menü-fix""
    This reverts commit 741c3d29987674468c91d3f6b5e0d0823060820f.

--- Assets/Scripts/WeaponControl.cs
+++ Assets/Scripts/WeaponControl.cs
@@ -239,6 +239,8 @@ public class WeaponControl : Bolt.EntityBehaviour<IPlayerState>
 
         Physics.Raycast(firstPersonCamera.transform.position, firstPersonCamera.transform.forward, out hit);
         if (hit.collider == null) return null;
+        var script = hit.collider.gameObject.GetComponent<FirstPersonController>();
+        if (script.entity.IsOwner) return null;
         return hit.collider.gameObject;
     }
 
@@ -249,6 +251,8 @@ public class WeaponControl : Bolt.EntityBehaviour<IPlayerState>
 
         Physics.Raycast(firstPersonCamera.transform.position, firstPersonCamera.transform.forward, out hit, range);
         if (hit.collider == null) return null;
+        var script = hit.collider.gameObject.GetComponent<FirstPersonController>();
+        if (script == null || script.entity.IsOwner) return null;
         return hit.collider.gameObject;
     }
 }
